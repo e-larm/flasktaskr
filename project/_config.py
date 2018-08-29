33e69236da9823eedbe10439e059267eab245917
Mwirigi Eric <mwirigieric@gmail.com>
2018-08-29 16:08:02 +0300

    update_01

--- project/_config.py
+++ project/_config.py
@@ -1,11 +1,10 @@
+# project/_config.py
 import os
 
 # grab the folder where this script lives
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
-USERNAME = 'admin'
-PASSWORD = 'admin'
 CSRF_ENABLED = True
 SECRET_KEY = 'myprecious'
 
